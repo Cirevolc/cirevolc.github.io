276864dd2c36f86183088cd9da291913e94f0085
Liang <liang@Liangs-MacBook-Pro.local>
2019-07-27 18:45:01 -0400

    graph1

--- cs5200/crime_rate_by_neighborhood_and_type/scatter.js
+++ cs5200/crime_rate_by_neighborhood_and_type/scatter.js
@@ -176,22 +176,22 @@ d3.csv("crime_rate_by_neighborhood_and_type.csv", function (data) {
   //   objects.selectAll(".dot").transition().duration(1000).attr("transform", transform);
   // }
 
-  function zoom() {
-    svg.select(".x.axis").call(xAxis);
-    svg.select(".y.axis").call(yAxis);
+  // function zoom() {
+  //   svg.select(".x.axis").call(xAxis);
+  //   svg.select(".y.axis").call(yAxis);
 
-    svg.selectAll(".dot")
-      .attr("transform", transform);
+  //   svg.selectAll(".dot")
+  //     .attr("transform", transform);
 
-    svg.selectAll(".dot2")
-      .attr("transform", transform2);
-  }
+  //   svg.selectAll(".dot2")
+  //     .attr("transform", transform2);
+  // }
 
   function transform(d) {
     return "translate(" + x(d[xCat]) + "," + y(d[yCat]) + ")";
   }
 
   function transform2(d) {
-    return "translate(" + x(d[xCat2]) + "," + y(d[yCat]) + ")";
+    return "translate(" + x(d[xCat2])-10 + "," + y(d[yCat])-10 + ")";
   }
 });
